9db0f4d7461e561d31cffe11207fe1ac9adad851
Mohan4477 <pandu11122251@gmail.com>
2025-05-12 09:07:20 +0530

    Time: 0 ms (100%), Space: 40.9 MB (10.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,11 @@
 class Solution {
+    int[] memo = new int[46];
     public int climbStairs(int n) {
+        if (n <= 2) return n;
 
-        
-        if (n <= 2){
-            return n;
-        }
-        else { 
-            int a = 1;
-            int b = 2;
-            for (int i=3;i<=n;i++){
-                int c = a + b;
-                a = b;
-                b = c;
-            }
-            return b;
-        }
+        if (memo[n] != 0) return memo[n];
+
+        memo[n] = climbStairs(n-1) + climbStairs(n-2);
+        return memo[n];
     }
 }
